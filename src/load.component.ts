0673b3616e4720aac5e3c5bc0646a0a5f71d98e6
christopher <christopher.kade@epitech.eu>
2017-06-08 18:07:24 +0200

    User can now change odd and even numbered squares by their color using the attributes 'colorEven', 'colorOdd'.

--- src/load.component.ts
+++ src/load.component.ts
@@ -7,7 +7,9 @@ import {LoadService} from './load.service';
   styleUrls: ['./load.component.css']
 })
 export class LoadComponent implements OnInit {
-  @Input() animate = true;
+  animate: boolean = false;
+  @Input() colorOdd: string;
+  @Input() colorEven: string;
 
   constructor(public loadService: LoadService) {}
 
@@ -15,6 +17,45 @@ export class LoadComponent implements OnInit {
     this.loadService.getValue().subscribe((status: boolean) => {
       this.animate = status;
     });
+
+    this.handleOddColor();
+    this.handleEventColor();
   }
 
+  /**
+   * Checks color code passed by property binding
+   */
+  handleOddColor() {
+    switch (this.colorOdd) {
+      case 'red':
+        this.colorOdd = '#F44336';
+        break;
+      case 'blue':
+        this.colorOdd = '#2196F3';
+        break;
+      case 'yellow':
+        this.colorOdd = '#FFEB3B';
+        break;
+      case 'teal':
+        this.colorOdd = '#009688';
+        break;
+    }
+  }
+
+  handleEventColor() {
+    switch (this.colorEven) {
+      case 'red':
+        this.colorEven = '#F44336';
+        break;
+      case 'blue':
+        this.colorEven = '#2196F3';
+        break;
+      case 'yellow':
+        this.colorEven = '#FFEB3B';
+        break;
+      case 'teal':
+        this.colorEven = '#009688';
+        break;
+    }
+  }
 }
